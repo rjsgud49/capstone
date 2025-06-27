288c366094504871cbee3e9cfe9114dbb8ebd4ca
rjsgud49 <qkrrjsgud49@gmail.com>
2025-06-27 20:08:53 +0900

    테스트 nigix

--- ROOMIT/src/Components/Header.jsx
+++ ROOMIT/src/Components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
         <header className="header">
             <div className="logo">
                 <img src="/ROMMITlogo.svg" alt="로고" className="logo-image" width="40px" />
-                ROOMIT
+                ROOMIT테스트
             </div>
 
             <nav className="nav">
