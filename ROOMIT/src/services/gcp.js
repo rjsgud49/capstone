7095c2a37d4a0f3d0d074986c4070ae6d1e1f48b
rjsgud49 <qkrrjsgud49@gmail.com>
2025-07-01 00:14:38 +0900

    수정

--- ROOMIT/src/services/gcp.js
+++ ROOMIT/src/services/gcp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const gcpAPI = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // '/api'
+    baseURL: import.meta.env.VITE_API_ALT_BASE_URL, // '/api'
   headers: {
     'Content-Type': 'application/json',
   }
